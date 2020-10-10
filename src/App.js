5e3320bf3aa21d901324d160cfb39487abf94f49
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-10-10 18:10:58 +0200

    update quest 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Mathew Brown" avatar="https://randomuser.me/api/portraits/men/72.jpg" online/>
+      
+      {/* <Contact name="Mathew Brown" avatar="https://randomuser.me/api/portraits/men/72.jpg" online/>
       <Contact name="Jayden Willis" avatar="https://randomuser.me/api/portraits/men/77.jpg"/>
-      <Contact name="Norma Sims" avatar="https://randomuser.me/api/portraits/women/2.jpg"/>
+      <Contact name="Norma Sims" avatar="https://randomuser.me/api/portraits/women/2.jpg"/> */}
+
+      <ContactList />
     </div>
   );
 }

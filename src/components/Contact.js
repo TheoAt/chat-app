7f5b1a9ce778ff7338755e7711d9653ffa8fe7d0
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-10-12 12:25:07 +0200

    update quest 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,31 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './Contact.css'
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt="avatar"/>
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <div className={props.online ? 'status-online' : 'status-offline'}></div>
-                    <p className="status-text">{props.online ? "online" : "offline"}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online
+        };
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt="avatar" />
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status">
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                        <p className="status-text" onClick={event => {
+                            const newStatus = !this.state.online;
+                            this.setState({ online: newStatus });
+                        }}>{this.state.online ? "online" : "offline"}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 Contact.propTypes = {
